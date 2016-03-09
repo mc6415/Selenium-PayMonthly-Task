8b4dfea0b4462cf20cd981def5eb6a2d1c576459
Michael Coombes <mc6415@gmail.com>
2016-03-09 16:32:11 +0000

    Added some basic error checking
    Added basic error checking for if you're on the right page and if the
    pay monthly button can't be found. Also implemented the .clear() method
    on the country search field

--- PayMonthly-Prices.py
+++ PayMonthly-Prices.py
@@ -7,11 +7,11 @@ from selenium.webdriver.support import expected_conditions as EC
 # This will get the country rates, done here so as not to repeat code for each country
 def getCountryRates(country):
     print '----------' + country + '----------'
-    # This finds the search box for the country, clicks on it to clear any text that might
+    # This finds the search box for the country, clears any text that may
     # already be there and searches for the country we are looking for
     countrySearch = browser.find_element_by_id('countryName')
-    countrySearch.click()
-    countrySearch.send_keys(country + Keys.RETURN)
+    countrySearch.clear()
+    countrySearch.send_keys(country + Keys.ENTER)
     
     # After performing the search, click the button to get the rates for pay monthly.
     try:
@@ -20,24 +20,37 @@ def getCountryRates(country):
         element = WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.ID, 'paymonthly'))
         )
-    finally:
+    except:
+        # If the pay monthly button is never found let the user know the reason for the error
+        print "Couldn't find the Pay Monthly Button"
+    else:
         payMonthlyButton = browser.find_element_by_id('paymonthly')
         payMonthlyButton.click()
         # Now we get the price table and print it out
         standardRates = browser.find_element_by_id('standardRatesTable')
         for row in standardRates.find_elements(By.TAG_NAME, 'tr'):
-            print row.text
+            print row.text            
     
 # define the URL and countries here as they have been supplied and do not change in this task
 url = "http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk"
+pageTitle = "O2 | International | International Caller Bolt On"
 countries = ['Canada', 'Germany', 'Iceland', 'Pakistan', 'Singapore', 'South Africa']
 
 # Starts up Firefox and navigates to the URL
 browser = webdriver.Firefox()
 browser.get(url)
 
-# Get country rates for the countries specified
-for country in countries:
-    getCountryRates(country)
-    
-browser.quit()
+# Check we are on the right page
+try:
+    element = WebDriverWait(browser, 10).until(
+        EC.title_contains("O2 | International")
+    )
+except:
+    print "On the wrong page exiting browser"
+    browser.quit()
+else:
+    # Get country rates for the countries specified
+    for country in countries:
+        getCountryRates(country)
+
+    browser.quit()
